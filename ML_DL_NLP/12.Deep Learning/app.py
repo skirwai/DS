091f373253b05b993a2e4e90e55f7450d17c6bdf
sandeep kirwai <sandeepkirwai@gmail.com>
2020-04-19 18:10:28 +0530

    docs
    All DS docs

---
+++ ML_DL_NLP/12.Deep Learning/app.py
@@ -0,0 +1,90 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Churn_Modelling.csv')
+X = dataset.iloc[:, 3:13].values
+y = dataset.iloc[:, 13].values
+
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X_1 = LabelEncoder()
+X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
+
+
+labelencoder_X_2 = LabelEncoder()
+X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
+
+
+onehotencoder = OneHotEncoder(categorical_features = [1])
+X = onehotencoder.fit_transform(X).toarray()
+X = X[:,1:]
+
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X = sc.fit_transform(X)
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
+
+
+
+import keras
+
+from keras.models import Sequential
+from keras.layers import Dense
+
+
+classifier = Sequential()
+
+
+classifier.add(Dense(units = 6, kernel_initializer='uniform', activation='relu', input_dim=11))
+
+
+classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
+
+
+
+classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+
+
+classifier.compile(optimizer='adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+
+classifier.fit(X_train, y_train, batch_size = 10, epochs =100)
+
+
+y_pred = classifier.predict(X_test)
+
+
+y_pred = (y_pred>0.5)
+
+
+from sklearn.metrics import confusion_matrix, classification_report
+cm = confusion_matrix(y_test, y_pred)
+print(classification_report(y_test, y_pred))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
